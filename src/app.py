782b62497766c36d87b572122caf099960c39ec4
Abrahan96 <perezccatamayoabrahan@gmail.com>
2025-03-14 00:41:05 -0500

    Creando la primera versión

---
+++ src/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from utils import insertar_cotizacion
+
+st.title("Cotización ML")
+
+# Formulario
+numero_cotiz = st.text_input("Número de Cotización", value="ML-00001")
+cliente = st.text_input("Cliente")
+direccion = st.text_input("Dirección")
+mecanico = st.text_input("Mecánico")
+subtotal = st.number_input("Subtotal", min_value=0.0)
+igv = subtotal * 0.18
+total = subtotal + igv
+
+st.write("IGV (18%): ", round(igv, 2))
+st.write("Total: ", round(total, 2))
+
+if st.button("Guardar Cotización"):
+    insertar_cotizacion(numero_cotiz, cliente, direccion, mecanico, subtotal, igv, total)
+    st.success("Cotización guardada con éxito ✅")
---
+++ src/utils.py
@@ -0,0 +1,21 @@
+from supabase import create_client, Client
+import os
+
+# Clave secreta de Supabase
+SUPABASE_URL = "https://eyemokwxswevabnuldej.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5ZW1va3d4c3dldmFibnVsZGVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5Mjg3NjAsImV4cCI6MjA1NzUwNDc2MH0.PrlwMQ4Exxuo1dGfclqmwBDnchRQ_7mQFi1hjiZKcno"
+
+# Conexión
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def insertar_cotizacion(numero_cotizacion, cliente, direccion, mecanico, subtotal, igv, total):
+    data = {
+        "numero_cotizacion": numero_cotizacion,
+        "cliente": cliente,
+        "direccion": direccion,
+        "mecanico": mecanico,
+        "subtotal": subtotal,
+        "igv": igv,
+        "total": total
+    }
+    supabase.table("cotizaciones").insert(data).execute()
