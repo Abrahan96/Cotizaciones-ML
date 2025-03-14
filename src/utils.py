ae035374b8707492ddfd578a6f23435597a28b32
Abrahan96 <perezccatamayoabrahan@gmail.com>
2025-03-14 12:34:08 -0500

    Creando def de crud

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,16 @@
 from supabase import create_client, Client
 import os
 
-# Conexión a Supabase
-url = "https://eyemokwxswevabnuldej.supabase.co"  # Tu URL de Supabase
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5ZW1va3d4c3dldmFibnVsZGVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5Mjg3NjAsImV4cCI6MjA1NzUwNDc2MH0.PrlwMQ4Exxuo1dGfclqmwBDnchRQ_7mQFi1hjiZKcno"  # API Key (service_role)
-supabase: Client = create_client(url, key)
+# Clave secreta de Supabase
+SUPABASE_URL = "https://eyemokwxswevabnuldej.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5ZW1va3d4c3dldmFibnVsZGVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5Mjg3NjAsImV4cCI6MjA1NzUwNDc2MH0.PrlwMQ4Exxuo1dGfclqmwBDnchRQ_7mQFi1hjiZKcno"
+
+# Conexión
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def insertar_cotizacion(numero_cotizacion, cliente, direccion, mecanico, subtotal, igv, total):
     data = {
-        "numero_cotizacion": numero_cotizacion,
+        "numero_cotiz": numero_cotizacion,
         "cliente": cliente,
         "direccion": direccion,
         "mecanico": mecanico,
@@ -16,9 +18,18 @@ def insertar_cotizacion(numero_cotizacion, cliente, direccion, mecanico, subtota
         "igv": igv,
         "total": total
     }
-    return supabase.table("cotizaciones").insert(data).execute()
-
-# Obtener todas las cotizaciones
+    supabase.table("cotizaciones").insert(data).execute()
+    
 def obtener_cotizaciones():
     response = supabase.table("cotizaciones").select("*").execute()
     return response.data
+
+# ACTUALIZAR COTIZACIÓN
+def actualizar_cotizacion(id_cotizacion, nuevos_datos):
+    response = supabase.table("cotizaciones").update(nuevos_datos).eq("id", id_cotizacion).execute()
+    return response
+
+# ELIMINAR COTIZACIÓN
+def eliminar_cotizacion(id_cotizacion):
+    response = supabase.table("cotizaciones").delete().eq("id", id_cotizacion).execute()
+    return response
